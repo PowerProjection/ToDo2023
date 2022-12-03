5fb909e956825cc57e8b4c64e359ed9f3556b617
PowerProjection <powerprojectionx@gmail.com>
2022-12-03 20:17:21 +0300

    desktop_comm

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Todos from './components/Todos';
 
 const todo_data = [
   {id:1, name:'Köpeği gezdir', isDone:false},
-  {id:2, name:'Araştırma yap', isDone:true},
+  {id:2, name:'Çiçekleri sula', isDone:true},
   {id:3, name:'Alışveriş yap', isDone:false},
   {id:4, name:'Kitap oku', isDone:false}
 ]
